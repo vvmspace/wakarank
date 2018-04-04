a6e407bd88c2626bdd29ef94325d11090a2ae17f
vvmsecured <iva9Yatoj>
2018-04-04 23:55:33 +0500

    It works

--- js/background.js
+++ js/background.js
@@ -10,7 +10,24 @@ class BG {
     }
 
     tick(){
+        $.getJSON('https://wakatime.com/api/v1/leaders', function (data) {
+            BG.wakaUpdate(data);
+        });
+    }
 
+    static  wakaUpdate(data){
+        let text = 'Log in';
+        let color = 'red';
+        if(data.current_user){
+            text = data.current_user.rank;
+            color = 'green';
+        }
+        chrome.browserAction.setBadgeText({
+            text: text.toString()
+        });
+        chrome.browserAction.setBadgeBackgroundColor({
+            color: color
+        });
     }
 
     initTicker(){
