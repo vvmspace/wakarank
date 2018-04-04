b612e791e8aabeb51fff150ffaebda77c96d230b
vvmsecured <iva9Yatoj>
2018-04-04 23:17:27 +0500

    Ticker every two minutes

--- js/background.js
+++ js/background.js
@@ -1,8 +1,23 @@
-let BG = {
-    init: function(){
+class BG {
+
+    constructor(){
+        this.interval = 2 * 60 * 1000;
+        this.initTicker();
+    }
+
+    static init(){
+        let bg = new BG();
+    }
+
+    tick(){
 
     }
-};
+
+    initTicker(){
+        this.tick();
+        setInterval(this.tick, this.interval);
+    }
+}
 
 $(function () {
    BG.init();
